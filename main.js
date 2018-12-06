cebcede25c351e0029b4c9d25521c41f8e0b509b
xXDuckXx <dacs2010@gmail.com>
2018-12-05 19:14:48 -0500

    displays artists name of every artist in the results

--- main.js
+++ main.js
@@ -2,10 +2,15 @@ function getId(id) {
     return document.getElementById(id)
 }
 
+function makeElement(tag) {
+    return document.createElement(tag)
+}
+
 const searchBar = getId("search-bar")
 const submitButton = getId("submit")
 
 submitButton.addEventListener('click', function(event){
+    // look into making every word capitalized
     console.log(searchBar.value)
     $.ajax({
         url: 'https://itunes.apple.com/search',
@@ -17,11 +22,18 @@ submitButton.addEventListener('click', function(event){
             data = JSON.parse(jsonFile)
             console.log(data)
             let dataDisp = getId("output")
-            let countDisp = document.createElement('p')
-            countDisp.innerText = `you got ${data.resultCount} hits`
+            let countDisp = makeElement('p')
+            dataDisp.innerHTML = ""
+            countDisp.innerText += `There are ${data.resultCount} hits for ${searchBar.value}`
             dataDisp.appendChild(countDisp)
 
-            // for (let item in data.results)
+            for (let item of data.results) {
+                let itemDisp = makeElement("div")
+                let itemTitle = makeElement("p")
+                itemTitle.innerText = item.artistName
+                itemDisp.appendChild(itemTitle)
+                dataDisp.appendChild(itemDisp)
+            }
         }
     })
 })
