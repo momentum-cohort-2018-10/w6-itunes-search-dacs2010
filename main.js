914433a24978d53bb3e5f799a8c4a7df30ab6dbb
xXDuckXx <dacs2010@gmail.com>
2018-12-09 09:12:41 -0500

    no longer searches with empty field, play button bubbles up proper src, still doesn't play however

--- main.js
+++ main.js
@@ -38,45 +38,49 @@ function mouseClick() {
 
 
 function searchEvent() {
-    $.get('https://itunes.apple.com/search',
-        {
-            term: searchBar.value,
-            media: 'music'
-        },
-        function (jsonFile) {
-            data = JSON.parse(jsonFile)
-            console.log(data)
-            let dataDisp = getId("output")
-            let countDisp = makeElement('p')
-            dataDisp.innerHTML = ""
-            countDisp.innerText += `There are ${data.resultCount} hits for "${searchBar.value}"`
-            dataDisp.appendChild(countDisp)
-            searchBar.value = ""
+    if (searchBar.value === "") {
+    }else{
+        $.get('https://itunes.apple.com/search',
+            {
+                term: searchBar.value,
+                media: 'music',
+            },
+            function (jsonFile) {
+                data = JSON.parse(jsonFile)
+                console.log(data)
+                let dataDisp = getId("output")
+                let countDisp = makeElement('p')
+                dataDisp.innerHTML = ""
+                countDisp.innerText += `There are ${data.resultCount} hits for "${searchBar.value}"`
+                dataDisp.appendChild(countDisp)
+                searchBar.value = ""
 
-            for (let item of data.results) {
-                // look into replace or any method to change the query for the image size
-                let itemArt = item.artworkUrl100
-                $('#output').append(`<div class="item"></div>`)
-                $('.item').append(`
-                    <img src="${item.artworkUrl100}"></img>
-
-                    <p>${item.artistName}</p>
-                    <p>${item.collectionName}</p>
-                    <p>${item.trackName}</p>
+                for (let item of data.results) {
+                    // look into replace or any method to change the query for the image size
+                    let itemSRC = item.previewUrl
+                    $('#output').append(`<div class="item-wrapper"></div>`)
+                    $('.item-wrapper').append(`
+                    <div class="item">
+                        <img src="${item.artworkUrl100}"></img>
+                        <img class="target" src="${item.previewUrl}" controls></img>
+                        <p>Track Name: ${item.trackName}</p>
+                        <p>Artist: ${item.artistName}</p>
+                        <p>Album: ${item.collectionName}</p>
+                        <p>Track Name: ${item.trackName}</p>
+                        
+                    </div>
                 `)
+                }
             }
-        }
-    )
+        )
+    }
 }
-// put a button into each item  
-// access the bubble up and execut the update command
-$('.item').on("click", function (event) {
-    console.log(event)
-} )
-// point to the id of the player...
-// listen for bubble up event then...
-// update the video nodes scr with the target
 
+
+$('#output').on("click", ".item", function (event) {
+    console.log(event)
+    $('#player').attr("src", event.target.src)
+} )
 
 
 
